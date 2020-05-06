699b1df536ae76fe44abb2d76e6eb208da8639a6
Ihor08 <ihor100500@ukr.net>
2020-05-06 11:33:06 +0300

    first commit

---
+++ lab32/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using System.IO;
+namespace ConsoleApplication1
+{
+    class Class1
+    {
+
+        static void Main()
+        {
+            string a = System.IO.File.ReadAllText(@"c:\input.txt");
+            char[] b = a.ToCharArray();
+           
+            
+            for (int i = 0; i < b.Length; i++)
+            {
+                if (i == 0 && b[i] == 's')
+                {
+                    for (int j = i; j < b.Length; j++)
+                    {
+                        if (b[j] == ' ')
+                        {
+                            Console.WriteLine();
+                            break;
+                        }
+                        Console.Write(b[j]);
+                    }
+
+                }
+                else {
+                    if (b[i] == 's' && b[i - 1] == ' ')
+                    {
+                        for (int j = i; j < b.Length; j++)
+                        {
+                            if (b[j] == ' ')
+                            {
+                                Console.WriteLine();
+                                break;
+                            }
+                            Console.Write(b[j]);
+                        }
+                    }
+                } 
+
+                }
+
+
+            
+           
+            
+
+        }
+    }
+}
